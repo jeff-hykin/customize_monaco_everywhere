7675c5899f4daeb202463fdfdad681b4f5bf978e
Jeff Hykin <jeff.hykin@gmail.com>
2025-05-28 13:50:11 -0500

    working

--- src/content.js
+++ src/content.js
@@ -1,1 +1,31 @@
-console.log('content.js')
+console.log(`content`)
+const isMacOS = navigator.userAgent.includes('Macintosh') || navigator.userAgent.includes('Mac OS X');
+console.debug(`isMacOS is:`,isMacOS)
+
+// 
+// hackerrank run buttons
+// 
+if (document.querySelector('.ui-btn.ui-btn-normal.ui-btn-secondary.pull-right.msR.hr-monaco-compile.hr-monaco__run-code.ui-btn-styled')) {
+    // ctrl+enter = basic run
+    // ctrl+shift+enter = submit
+    document.addEventListener('keydown', function(event) {
+        if (isMacOS && event.ctrlKey && event.key === 'Enter') {
+            document.querySelector('.ui-btn.ui-btn-normal.ui-btn-secondary.pull-right.msR.hr-monaco-compile.hr-monaco__run-code.ui-btn-styled').click();
+        }
+    })
+    document.addEventListener('keydown', function(event) {
+        if (isMacOS && event.ctrlKey && event.shiftKey && event.key === 'Enter') {
+            document.querySelector('.ui-btn.ui-btn-normal.ui-btn-primary.pull-right.hr-monaco-submit.ui-btn-styled').click();
+        }
+    })
+}
+
+// Inject a script tag into the page
+const script = document.createElement('script');
+script.src = chrome.runtime.getURL('injected.js');
+script.onload = function () {
+  this.remove(); // clean up after loading
+};
+(document.head || document.documentElement).appendChild(script);
+
+
